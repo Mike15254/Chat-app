1d42a3d1d9b9a5477856f351f04c90295612338f
Mike15254 <mike8max10@gmail.com>
2023-03-14 22:53:06 +0300

    Create new files and components for a Chat App with login functionality and real-time messaging

---
+++ Front-end/chat/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import Login from './lib/Login.svelte';
+	import Messages from './lib/messages.svelte';
+	import { currentUser } from './lib/pocketbase';
+</script>
+
+<h1>Chat App</h1>
+
+<Login />
+
+{#if $currentUser}
+	<Messages />
+{/if}
+
